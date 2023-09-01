41fe1d687efd300d4fb75be949221bb5370c0858
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2023-09-01 12:52:22 +0800

    1.0 所有修改准备

--- release/script/groovy/atom/calCluster.groovy
+++ release/script/groovy/atom/calCluster.groovy
@@ -4,6 +4,7 @@ import com.jtool.atom.Atom
 import com.jtool.atom.IAtom
 import com.jtool.lmp.Dump
 import com.jtool.rareevent.atom.ABOOPSolidChecker
+import com.jtool.rareevent.atom.MainTypeClusterSizeCalculator
 
 import java.util.stream.Collectors
 
@@ -25,10 +26,14 @@ import java.util.stream.Collectors
 final def filterDir = 'lmp/.stableglass-out/';
 
 final def dump = Dump.read('lmp/.stableglass-in/dump');
-final def checker = new ABOOPSolidChecker().setRNearestMul(2.2).setConnectThreshold(0.88).setSolidThreshold(13);
+final def calculator = new MainTypeClusterSizeCalculator(
+    new ABOOPSolidChecker().setRNearestMul(2.2).setConnectThreshold(0.83).setSolidThreshold(25),
+    new ABOOPSolidChecker().setRNearestMul(1.5).setConnectThreshold(0.84).setSolidThreshold(7),
+    2
+);
 
 def filterDump = dump.parallelStream().map {subDump ->
-    def isSolid = subDump.getMPC().withCloseable {checker.checkSolid(it)}
+    def isSolid = subDump.getMPC().withCloseable {calculator.getIsSolid_(it, subDump)}
     int j = 0;
     subDump.opt().collect {IAtom atom -> isSolid[j++] ? new Atom(atom).setType(atom.type()+2) : atom};
 }.collect(Collectors.toList());
