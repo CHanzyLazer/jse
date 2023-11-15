8364400e03aea5ae6e68429c56eba1313b5968f3
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2023-11-15 20:33:09 +0800

    voronoi 更多例子

--- release/script/groovy/test/mpc/testVoronoi1.groovy
+++ release/script/groovy/test/mpc/testVoronoi1.groovy
@@ -11,21 +11,34 @@ import static jtool.code.UT.Par.*
 import static jtool.code.UT.Code.*
 
 def dump = XDATCAR.read('lmp/.lll-in/XDATCAR');
+final int atomNum = dump.atomNum();
+final int atomTypeNum = dump.atomTypeNum();
+
+//dump = dump[range(0, dump.size(), 20)];
 // 此数据需要每帧都进行一次 wrap
 for (data in dump) data.opt().wrap2this();
 
 // 统计原子体积和配位数的变化趋势
-def allAtomicVolume = ArrayLists.from(dump.size(), {Vectors.zeros(dump.atomNum())})
-def coordination    = ArrayLists.from(dump.size(), {Vectors.zeros(dump.atomNum())})
+def allAtomicVolume = ArrayLists.from(dump.size(), {Vectors.zeros(atomNum)});
+def allCoordination = ArrayLists.from(dump.size(), {Vectors.zeros(atomNum)});
+def neighborAtomicVolume = ArrayLists.from(dump.size(), {Vectors.zeros(atomNum)});
+def neighborCoordination = ArrayLists.from(dump.size(), {Vectors.zeros(atomNum)});
 
-// 借助一下 MFPC 将原子按照相同的 id 排序；虽然对于 XDATCAR 是不必要的
 UT.Timer.tic();
-try (def mfpc = dump.getMFPC(0.001)) {
-    parfor(dump.size()) {int i ->
-        try (def mpc = mfpc.getMPC(i)) {
-            def voronoi = mpc.calVoronoi();
-            allAtomicVolume[i].fill({int j -> voronoi[j].atomicVolume()});
-            coordination   [i].fill({int j -> voronoi[j].coordination()});
+parfor(dump.size()) {int i ->
+    try (def mpc = dump[i].getMPC()) {
+        def voronoi = mpc.calVoronoi();
+        def subAllAtomicVolume = allAtomicVolume[i];
+        def subAllCoordination = allCoordination[i];
+        subAllAtomicVolume.fill({int j -> voronoi[j].atomicVolume()});
+        subAllCoordination.fill({int j -> voronoi[j].coordination()});
+        // 获取近邻平均的结果
+        def subNeighborAtomicVolume = neighborAtomicVolume[i];
+        def subNeighborCoordination = neighborCoordination[i];
+        for (int j in range(atomNum)) {
+            def nl = mpc.getNeighborList(j);
+            subNeighborAtomicVolume[j] =  subAllAtomicVolume[nl].mean();
+            subNeighborCoordination[j] =  subAllCoordination[nl].mean();
         }
     }
 }
@@ -35,20 +48,24 @@ UT.Timer.toc('voronoi');
 // 绘图
 def plt1 = Plotters.get();
 def plt2 = Plotters.get();
+def plt3 = Plotters.get();
 
 def data = dump[0];
-def typeLegend = LogicalVector.zeros(data.atomTypeNum());
-for (j in range(data.atomNum())) {
+def typeLegend = LogicalVector.zeros(atomTypeNum);
+for (j in atomNum<..0) {
     def type = data.pickAtom(j).type();
-    plt1.plot(1..dump.size(), (allAtomicVolume*.get(j)), typeLegend[type-1] ? null : "type-$type").color(type);
-    plt2.plot(1..dump.size(), (coordination*.get(j))   , typeLegend[type-1] ? null : "type-$type").color(type);
+    plt1.plot(1..dump.size(), (allAtomicVolume*.get(j))     , typeLegend[type-1] ? null : "type-$type").color(type).width(type==2 ? 2.5 : 1.7);
+    plt2.plot(1..dump.size(), (neighborCoordination*.get(j)), typeLegend[type-1] ? null : "type-$type").color(type).width(type==2 ? 2.5 : 1.7);
+    plt3.plot((neighborCoordination*.get(j)), (allAtomicVolume*.get(j)), typeLegend[type-1] ? null : "type-$type").lineType('none').color(type).marker(type==1 ? 'o' : 's').size(type==2 ? 12 : 8);
     typeLegend[type-1] = true;
 }
 
 plt1.xrange(0, dump.size()+1);
 plt2.xrange(0, dump.size()+1);
-plt1.xlabel('frame').ylabel('allAtomicVolume');
-plt2.xlabel('frame').ylabel('coordination');
+plt1.xlabel('frame').ylabel('atomicVolume');
+plt2.xlabel('frame').ylabel('neighborAtomicVolume');
+plt3.xlabel('neighborCoordination').ylabel('atomicVolume');
 plt1.show();
 plt2.show();
+plt3.show();
 
---
+++ release/script/groovy/test/mpc/testVoronoi1b.groovy
@@ -0,0 +1,56 @@
+package test.mpc
+
+import jtool.code.UT
+import jtool.code.collection.ArrayLists
+import jtool.math.vector.LogicalVector
+import jtool.math.vector.Vectors
+import jtool.plot.Plotters
+import jtool.vasp.XDATCAR
+
+import static jtool.code.UT.Par.*
+import static jtool.code.UT.Code.*
+
+def dump = XDATCAR.read('lmp/.lll-in/XDATCAR');
+final int atomNum = dump.atomNum();
+
+//dump = dump[range(0, dump.size(), 20)];
+// 此数据需要每帧都进行一次 wrap
+for (data in dump) data.opt().wrap2this();
+
+// 统计原子体积和配位数的变化趋势
+def allAtomicVolume = ArrayLists.from(dump.size(), {Vectors.zeros(atomNum)});
+def allCoordination = ArrayLists.from(dump.size(), {Vectors.zeros(atomNum)});
+
+UT.Timer.tic();
+parfor(dump.size()) {int i ->
+    try (def mpc = dump[i].getMPC()) {
+        def voronoi = mpc.calVoronoi();
+        def subAllAtomicVolume = allAtomicVolume[i];
+        def subAllCoordination = allCoordination[i];
+        subAllAtomicVolume.fill({int j -> voronoi[j].atomicVolume()});
+        subAllCoordination.fill({int j -> voronoi[j].coordination()});
+    }
+}
+UT.Timer.toc('voronoi');
+
+// 直接对时间进行平均
+def meanAtomicVolume = Vectors.zeros(atomNum);
+def meanCoordination = Vectors.zeros(atomNum);
+for (i in range(dump.size())) {
+    meanAtomicVolume.plus2this(allAtomicVolume[i]);
+    meanCoordination.plus2this(allCoordination[i]);
+}
+meanAtomicVolume.div2this(dump.size());
+meanCoordination.div2this(dump.size());
+
+
+// 绘图
+def plt = Plotters.get();
+def type = Vectors.from((dump[0].asList()*.type()));
+plt.plot(meanCoordination[type.equal(2)], meanAtomicVolume[type.equal(2)], 'type-2').color(2).lineType('none').marker('o');
+plt.plot(meanCoordination[type.equal(1)], meanAtomicVolume[type.equal(1)], 'type-1').color(1).lineType('none').marker('s');
+plt.xlabel('coordination').ylabel('atomicVolume');
+plt.show();
+
+println(meanAtomicVolume[type.equal(1)]);
+
