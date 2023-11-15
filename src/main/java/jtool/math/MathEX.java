478e8826697fddfe042e786783a71c1ead87e470
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2023-11-15 15:12:35 +0800

    更新新版 voronoi，性能大幅提升

--- src/main/java/jtool/math/MathEX.java
+++ src/main/java/jtool/math/MathEX.java
@@ -8,7 +8,7 @@ import jtool.code.functional.*;
 import jtool.math.function.Func2;
 import jtool.math.function.Func3;
 import jtool.parallel.ParforThreadPool;
-import jtoolex.voronoi.Geometry;
+import jtoolex.algorithm.Geometry;
 import net.jafama.FastMath;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -525,11 +525,55 @@ public class MathEX {
     /// Methods of Graph usage
     public static class Graph {
         
-        /** wrapper of {@link Geometry} stuffs */
-        public static double area(IXYZ aA, IXYZ aB, IXYZ aC) {return Geometry.area(aA, aB, aC);}
-        public static double leftOfPlane(IXYZ aA, IXYZ aB, IXYZ aC, IXYZ aD) {return Geometry.leftOfPlane(aA, aB, aC, aD);}
-        public static double inSphere(IXYZ aA, IXYZ aB, IXYZ aC, IXYZ aD, IXYZ aE) {return Geometry.inSphere(aA, aB, aC, aD, aE);}
-        public static XYZ centerSphere(IXYZ aA, IXYZ aB, IXYZ aC, IXYZ aD) {return Geometry.centerSphere(aA, aB, aC, aD);}
+        /**
+         * 为了可读性以及减少重复代码，这里使用会创建临时变量 {@link XYZ} 的
+         * @author CHanzy
+         * @return A, B, C 三点组成的三角形的面积，永远为正数
+         */
+        public static double area(IXYZ aA, IXYZ aB, IXYZ aC) {
+            XYZ tA = XYZ.toXYZ(aA);
+            XYZ tAB = aB.minus(tA);
+            XYZ tAC = aC.minus(tA);
+            tAB.cross2this(tAC);
+            return 0.5 * tAB.norm();
+        }
+        /**
+         * 确定点 D 是否位于由点 A、B 和 C 定义的平面的左侧。假定从平面的右侧看，ABC 满足逆时针的顺序。
+         * @return 如果在左边则为正，右边则为负，刚好在平面上则为 0
+         */
+        public static double leftOfPlane(IXYZ aA, IXYZ aB, IXYZ aC, IXYZ aD) {
+            return Geometry.leftOfPlane(
+                  aA.x(), aA.y(), aA.z()
+                , aB.x(), aB.y(), aB.z()
+                , aC.x(), aC.y(), aC.z()
+                , aD.x(), aD.y(), aD.z());
+        }
+        /**
+         * 确定点 E 是否位于由点 A、B、C 和 D 定义的球体的内部。假定 {@code leftOfPlane(A, B, C, D) > 0}。
+         * @return 如果在内部则为正，外部则为负，刚好在球面上则为 0
+         */
+        public static double inSphere(IXYZ aA, IXYZ aB, IXYZ aC, IXYZ aD, IXYZ aE) {
+            return Geometry.inSphere(
+                  aA.x(), aA.y(), aA.z()
+                , aB.x(), aB.y(), aB.z()
+                , aC.x(), aC.y(), aC.z()
+                , aD.x(), aD.y(), aD.z()
+                , aE.x(), aE.y(), aE.z());
+        }
+        /**
+         * 计算由点 A，B，C 和 D 定义的球的中心。假定 {@code leftOfPlane(A, B, C, D) > 0}。
+         * @return 球心 XYZ 坐标
+         */
+        public static XYZ centerSphere(IXYZ aA, IXYZ aB, IXYZ aC, IXYZ aD) {
+            XYZ rCenter = new XYZ(0.0, 0.0, 0.0);
+            Geometry.centerSphere(
+                  aA.x(), aA.y(), aA.z()
+                , aB.x(), aB.y(), aB.z()
+                , aC.x(), aC.y(), aC.z()
+                , aD.x(), aD.y(), aD.z()
+                , rCenter);
+            return rCenter;
+        }
         
         /**
          * Get all the Intersection Points of the ray between 2D box
