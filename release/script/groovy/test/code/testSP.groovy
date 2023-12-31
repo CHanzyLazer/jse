cd1885d0caf1b2bc7fc902154add99a3196fb443
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2023-12-31 18:58:15 +0800

    似乎新版已经改善了 java Number 的问题，这里再增加了对于 BigDecimal 的自动转换，应该已经解决所有问题

--- release/script/groovy/test/code/testSP.groovy
+++ release/script/groovy/test/code/testSP.groovy
@@ -18,36 +18,34 @@ import jtool.math.vector.Vectors
 
 
 /** 系统中自带的 python 库使用 */
-import static jtool.code.UT.Math.*
-
+np = SP.Python.importModule('numpy');
 plt = SP.Python.importModule('matplotlib.pyplot');
 
-def x = linspace(0.0, 2.0*pi, 20);
-def y = sin(x);
+def x = np.linspace(0.0, 2.0*np.pi, 20);
+def y = np.sin(x);
 
-plt.plot(x.data(), y.data());
+plt.plot(x, y);
 plt.show();
 
 
 /** ase 库的使用 */
-//SP.Python.installAse();
+////SP.Python.installAse();
 //SP.Python.runText('from ase import Atoms');
 //double a = 3.55;
 //def Atoms = SP.Python.getClass('Atoms');
 //def atoms = Atoms('Ni4',
-//                  [cell: [MathEX.Fast.sqrt(2.0) * a, MathEX.Fast.sqrt(2.0) * a, 1.0, 90, 90, 120] as double[],
-//                   pbc: [1, 1, 0] as double[],
+//                  [cell: [MathEX.Fast.sqrt(2.0) * a, MathEX.Fast.sqrt(2.0) * a, 1.0, 90, 90, 120],
+//                   pbc: [1, 1, 0],
 //                   scaled_positions: [[0, 0, 0],
 //                                      [0.5, 0, 0],
 //                                      [0, 0.5, 0],
-//                                      [0.5, 0.5, 0]] as double[][]
+//                                      [0.5, 0.5, 0]]
 //                  ]);
 //atoms.center(vacuum: 5.0, axis: 2)
 //
 //println(atoms.cell);
 //println(atoms.positions);
 //println(atoms[0]);
-//SP.Python.setValue('atoms', atoms.unwrap());
+//SP.Python.setValue('atoms', atoms);
 //SP.Python.runText('print(atoms.positions)');
-
 
