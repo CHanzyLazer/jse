3c1969c33ebaea9c8d97a9387500ad15e764cfb6
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2024-04-21 00:14:46 +0800

    增加元素名称列表，可以通过原子序数快速获取名称

--- src/main/java/jse/code/CS.java
+++ src/main/java/jse/code/CS.java
@@ -103,6 +103,21 @@ public class CS {
     public final static Pattern COMMA = Pattern.compile("\\s*,\\s*");
     public final static Pattern COMMA_OR_BLANKS = Pattern.compile("\\s*[,\\s]\\s*");
     
+    /** Boltzmann constant */
+    public final static double K_B = 0.0000861733262; // eV / K
+    
+    /** All atom name, start from 0 */
+    public final static String[] ATOM_TYPE_NAMES = {
+        "H" , "He",
+        "Li", "Be", "B" , "C" , "N" , "O" , "F" , "Ne",
+        "Na", "Mg", "Al", "Si", "P" , "S" , "Cl", "Ar",
+        "K" , "Ca", "Sc", "Ti", "V" , "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr",
+        "Rb", "Sr", "Y" , "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I" , "Xe",
+        "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf",
+        "Ta", "W" , "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn",
+        "Fr", "Ra", "Ac", "Th", "Pa", "U" , "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf",
+        "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"
+    };
     /** All atomic mass */
     public final static Map<String, Double> MASS = (new ImmutableMap.Builder<String, Double>())
         .put("H" , 1.00794)
@@ -587,9 +602,6 @@ public class CS {
         .put("Ts", 2.00)
         .put("Og", 2.00)
         .build();
-    
-    /** Boltzmann constant */
-    public final static double K_B = 0.0000861733262; // eV / K
     
     
     /** SystemExecutor Stuffs */
