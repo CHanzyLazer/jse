a5e57094b4a55d046af448f901726b8598eaab5e
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2024-05-16 21:39:17 +0800

    现在改为 SLURM 下自动开启 java.awt.headless，简单实现

--- src/main/java/jse/code/UT.java
+++ src/main/java/jse/code/UT.java
@@ -879,7 +879,6 @@ public class UT {
             return null;
         }
         
-        
         /**
          * 针对 lammps 等软件输出文件中存在的使用空格分割的数据专门优化的读取操作，
          * 使用 groovy-json 中现成的 parseDouble 等方法，总体比直接 split 并用 java 的 parseDouble 快一倍以上；
