c687dfbe63545aa38536f9b213877999441f0bde
CHanzyLazer <55473143+CHanzyLazer@users.noreply.github.com>
2024-11-29 13:22:52 +0800

    规范化变量名称

--- src/nnap/java/jsex/nnap/Trainer.java
+++ src/nnap/java/jsex/nnap/Trainer.java
@@ -30,13 +30,40 @@ import java.util.Map;
  */
 @ApiStatus.Experimental
 public class Trainer {
+    protected final static int[] DEFAULT_HIDDEN_DIMS = {20, 20};
+    protected final static boolean DEFAULT_BN_LAYER = true;
+    /** 全局记录 python 中的变量名称 */
+    protected final static String
+          CLASS_SINGLE_MODEL    = "__NNAP_TRAINER_SingleEnergyModel__"
+        , CLASS_TOTAL_MODEL     = "__NNAP_TRAINER_TotalEnergyModel__"
+        , FN_TRAIN_STEP         = "__NNAP_TRAINER_train_step__"
+        , VAL_MODEL             = "__NNAP_TRAINER_model__"
+        , VAL_OPTIMIZER         = "__NNAP_TRAINER_optimizer__"
+        , VAL_LOSS_FN           = "__NNAP_TRAINER_loss_fn__"
+        , VAL_TRAIN_BASE        = "__NNAP_TRAINER_train_base__"
+        , VAL_TRAIN_BASE_J      = "__NNAP_TRAINER_train_base_J__"
+        , VAL_TRAIN_ATOM_NUM    = "__NNAP_TRAINER_train_atom_num__"
+        , VAL_TRAIN_ATOM_NUM_J  = "__NNAP_TRAINER_train_atom_num_J__"
+        , VAL_TRAIN_ENG         = "__NNAP_TRAINER_train_eng__"
+        , VAL_TRAIN_ENG_J       = "__NNAP_TRAINER_train_eng_J__"
+        , VAL_NORM_VEC          = "__NNAP_TRAINER_norm_vec__"
+        , VAL_NORM_VEC_J        = "__NNAP_TRAINER_norm_vec_J__"
+        , VAL_NTYPES            = "__NNAP_TRAINER_ntypes__"
+        , VAL_BN_LAYERS         = "__NNAP_TRAINER_bn_layers__"
+        , VAL_HIDDEN_DIMS       = "__NNAP_TRAINER_hidden_dims__"
+        , VAL_INPUT_DIMS        = "__NNAP_TRAINER_input_dims__"
+        , VAL_SUB_NORM_VEC      = "__NNAP_TRAINER_sub_norm_vec__"
+        , VAL_SUB_BASE          = "__NNAP_TRAINER_sub_base__"
+        , VAL_I                 = "__NNAP_TRAINER_i__"
+        ;
+    
     public static class Conf {
         /** 全局初始化脚本，默认为设置串行；经过测试并行训练并不会明显更快 */
         public static String INIT_SCRIPT = "torch.set_num_threads(1); torch.set_num_interop_threads(1)";
         
         /** 创建获取单粒子能量的模型的类，修改此值来实现自定义模型 */
         public static String SINGLE_MODEL_SCRIPT =
-            "class __NNAP_TRAINER_SingleEnergyModel__(torch.nn.Module):\n" +
+            "class "+CLASS_SINGLE_MODEL+"(torch.nn.Module):\n" +
             "    def __init__(self, input_dim, hidden_dims, bn_layer):\n" +
             "        super().__init__()\n" +
             "        self.in_layer = torch.nn.Linear(input_dim, hidden_dims[0])\n" +
@@ -55,12 +82,12 @@ public class Trainer {
         
         /** 创建获取总能量的模型的类，修改此值来实现自定义模型；注意这里约定返回每原子能量，可以方便拟合以及 loss 函数的设计 */
         public static String TOTAL_MODEL_SCRIPT =
-            "class __NNAP_TRAINER_TotalEnergyModel__(torch.nn.Module):\n" +
+            "class "+CLASS_TOTAL_MODEL+"(torch.nn.Module):\n" +
             "    def __init__(self, input_dims, hidden_dims, bn_layers, ntypes=1):\n" +
             "        super().__init__()\n" +
             "        self.input_dims = input_dims\n" +
             "        self.ntypes = ntypes\n" +
-            "        self.sub_models = torch.nn.ModuleList([__NNAP_TRAINER_SingleEnergyModel__(input_dims[i], hidden_dims[i], bn_layers[i]) for i in range(ntypes)])\n" +
+            "        self.sub_models = torch.nn.ModuleList(["+CLASS_SINGLE_MODEL+"(input_dims[i], hidden_dims[i], bn_layers[i]) for i in range(ntypes)])\n" +
             "    \n" +
             "    def forward(self, x, atom_numbers):\n" +
             "        ylen = len(atom_numbers)\n" +
@@ -74,15 +101,15 @@ public class Trainer {
         
         /** 训练一步的脚本函数，修改此值来实现自定义训练算法；默认使用 LBFGS 训练 */
         public static String TRAIN_STEP_SCRIPT =
-            "def __NNAP_TRAINER_train_step__():\n" +
+            "def "+FN_TRAIN_STEP+"():\n" +
             "   def closure():\n" +
-            "       __NNAP_TRAINER_optimizer__.zero_grad()\n" +
-            "       pred = __NNAP_TRAINER_model__(__NNAP_TRAINER_train_data_base__, __NNAP_TRAINER_train_data_atom_num__)\n" +
-            "       loss = __NNAP_TRAINER_loss_fn__(pred, __NNAP_TRAINER_train_data_eng__)\n" +
+            "       "+VAL_OPTIMIZER+".zero_grad()\n" +
+            "       pred = "+VAL_MODEL+"("+VAL_TRAIN_BASE+", "+VAL_TRAIN_ATOM_NUM+")\n" +
+            "       loss = "+VAL_LOSS_FN+"(pred, "+VAL_TRAIN_ENG+")\n" +
             "       loss.backward()\n" +
             "       return loss\n" +
             "   loss = closure()\n" +
-            "   __NNAP_TRAINER_optimizer__.step(closure)\n" +
+            "   "+VAL_OPTIMIZER+".step(closure)\n" +
             "   return loss.item()";
     }
     static {
@@ -92,17 +119,15 @@ public class Trainer {
         SP.Python.exec(Conf.TOTAL_MODEL_SCRIPT);
         SP.Python.exec(Conf.TRAIN_STEP_SCRIPT);
     }
-    protected final static int[] DEFAULT_HIDDEN_DIMS = {20, 20};
-    protected final static boolean DEFAULT_BN_LAYER = true;
     
     protected final String[] mSymbols;
     protected final IVector mRefEngs;
     protected final Basis.IBasis[] mBasis;
-    protected final DoubleList[] mTrainDataBase; // 按照种类排序，然后内部是可扩展的具体数据，最后一列增加对应的能量的索引；现在使用这种 DoubleList 展开的形式存
-    protected final DoubleList mTrainDataEng = new DoubleList(64);
-    protected final IntList mTrainDataAtomNum = new IntList(64);
+    protected final DoubleList[] mTrainBase; // 按照种类排序，然后内部是可扩展的具体数据，最后一列增加对应的能量的索引；现在使用这种 DoubleList 展开的形式存
+    protected final DoubleList mTrainEng = new DoubleList(64);
+    protected final IntList mTrainAtomNum = new IntList(64);
     protected final Vector[] mNormVec;
-    protected final RowMatrix[] mTrainDataBaseMat; // mTrainDataBase 的实际值，当然这个只是缓存结果
+    protected final RowMatrix[] mTrainBaseMat; // mTrainDataBase 的实际值，当然这个只是缓存结果
     protected boolean mModelInited = false;
     protected final Map<String, ?> mModelSetting;
     protected boolean mOptimizerInited = false;
@@ -118,11 +143,11 @@ public class Trainer {
         for (int i = 0; i < mSymbols.length; ++i) {
             mNormVec[i] = Vector.zeros(mBasis[i].rowNumber()*mBasis[i].columnNumber());
         }
-        mTrainDataBase = new DoubleList[mSymbols.length];
+        mTrainBase = new DoubleList[mSymbols.length];
         for (int i = 0; i < mSymbols.length; ++i) {
-            mTrainDataBase[i] = new DoubleList(64);
+            mTrainBase[i] = new DoubleList(64);
         }
-        mTrainDataBaseMat = new RowMatrix[mSymbols.length];
+        mTrainBaseMat = new RowMatrix[mSymbols.length];
         mModelSetting = aModelSetting;
     }
     public Trainer(String[] aSymbols, IVector aRefEngs, Basis.IBasis aBasis, Map<String, ?> aModelSetting) {this(aSymbols, aRefEngs, repeatBasis_(aBasis, aSymbols.length), aModelSetting);}
@@ -144,7 +169,7 @@ public class Trainer {
             final List<?> tSubDims = tHiddenDims;
             tHiddenDims = NewCollections.from(mSymbols.length, i -> tSubDims);
         }
-        @Nullable Object tBnLayers = UT.Code.getWithDefault(mModelSetting, null, "bn_layers", "bn");
+        @Nullable Object tBnLayers = UT.Code.getWithDefault(mModelSetting, null, "bn_layers", "bn_layer", "bn");
         if (tBnLayers == null) {
             tBnLayers = NewCollections.from(mSymbols.length, i -> DEFAULT_BN_LAYER);
         }
@@ -152,33 +177,33 @@ public class Trainer {
             final Boolean tSubBnLayer = (Boolean)tBnLayers;
             tBnLayers = NewCollections.from(mSymbols.length, i -> tSubBnLayer);
         }
-        SP.Python.setValue("__NNAP_TRAINER_input_dims__", NewCollections.map(mBasis, base -> base.rowNumber()*base.columnNumber()));
-        SP.Python.setValue("__NNAP_TRAINER_hidden_dims__", tHiddenDims);
-        SP.Python.setValue("__NNAP_TRAINER_bn_layers__", tBnLayers);
-        SP.Python.setValue("__NNAP_TRAINER_ntypes__", mSymbols.length);
-        SP.Python.exec("__NNAP_TRAINER_model__ = __NNAP_TRAINER_TotalEnergyModel__(__NNAP_TRAINER_input_dims__, __NNAP_TRAINER_hidden_dims__, __NNAP_TRAINER_bn_layers__, ntypes=__NNAP_TRAINER_ntypes__).double()");
-        SP.Python.removeValue("__NNAP_TRAINER_ntypes__");
-        SP.Python.removeValue("__NNAP_TRAINER_bn_layers__");
-        SP.Python.removeValue("__NNAP_TRAINER_hidden_dims__");
-        SP.Python.removeValue("__NNAP_TRAINER_input_dims__");
+        SP.Python.setValue(VAL_INPUT_DIMS, NewCollections.map(mBasis, base -> base.rowNumber()*base.columnNumber()));
+        SP.Python.setValue(VAL_HIDDEN_DIMS, tHiddenDims);
+        SP.Python.setValue(VAL_BN_LAYERS, tBnLayers);
+        SP.Python.setValue(VAL_NTYPES, mSymbols.length);
+        SP.Python.exec(VAL_MODEL+" = "+CLASS_TOTAL_MODEL+"("+VAL_INPUT_DIMS+", "+VAL_HIDDEN_DIMS+", "+VAL_BN_LAYERS+", ntypes="+VAL_NTYPES+").double()");
+        SP.Python.removeValue(VAL_NTYPES);
+        SP.Python.removeValue(VAL_BN_LAYERS);
+        SP.Python.removeValue(VAL_HIDDEN_DIMS);
+        SP.Python.removeValue(VAL_INPUT_DIMS);
     }
     /** 重写实现自定义优化器创建，默认采用 LBFGS */
     protected void initOptimizer() {
-        SP.Python.exec("__NNAP_TRAINER_optimizer__ = torch.optim.LBFGS(__NNAP_TRAINER_model__.parameters(), history_size=100, max_iter=5, line_search_fn='strong_wolfe')");
+        SP.Python.exec(VAL_OPTIMIZER+" = torch.optim.LBFGS("+VAL_MODEL+".parameters(), history_size=100, max_iter=5, line_search_fn='strong_wolfe')");
     }
     /** 重写实现自定义损失函数创建，这里目前默认采用 SmoothL1Loss */
     protected void initLossFn() {
-        SP.Python.exec("__NNAP_TRAINER_loss_fn__ = torch.nn.SmoothL1Loss()");
+        SP.Python.exec(VAL_LOSS_FN+" = torch.nn.SmoothL1Loss()");
     }
     /** 重写实现自定义归一化方案 */
     protected void initNormVec() {
-        for (int i = 0; i < mTrainDataBase.length; ++i) {
+        for (int i = 0; i < mTrainBase.length; ++i) {
             mNormVec[i].fill(0.0);
-            for (IVector tRow : mTrainDataBaseMat[i].rows()) {
+            for (IVector tRow : mTrainBaseMat[i].rows()) {
                 IVector tRowData = tRow.subVec(0, mNormVec[i].size());
                 mNormVec[i].operation().operate2this(tRowData, (lhs, rhs) -> lhs + Math.abs(rhs));
             }
-            mNormVec[i].div2this(mTrainDataBaseMat[i].rowNumber());
+            mNormVec[i].div2this(mTrainBaseMat[i].rowNumber());
         }
     }
     /** 开始训练模型，这里直接训练给定的步数 */
@@ -200,48 +225,48 @@ public class Trainer {
         }
         if (aPrintLog) System.out.println("Init train data...");
         // 初始化矩阵数据
-        for (int i = 0; i < mTrainDataBase.length; ++i) {
+        for (int i = 0; i < mTrainBase.length; ++i) {
             final int tColNum = mBasis[i].rowNumber()*mBasis[i].columnNumber() + 1;
-            final int tRowNum = mTrainDataBase[i].size() / tColNum;
-            mTrainDataBaseMat[i] = new RowMatrix(tRowNum, tColNum, mTrainDataBase[i].internalData());
+            final int tRowNum = mTrainBase[i].size() / tColNum;
+            mTrainBaseMat[i] = new RowMatrix(tRowNum, tColNum, mTrainBase[i].internalData());
         }
         // 重新构建归一化向量
         initNormVec();
         // 构造 torch 数据，这里直接把数据放到 python 环境中变成一个 python 的全局变量
-        SP.Python.setValue("__NNAP_TRAINER_norm_vec_j__", mNormVec);
-        SP.Python.exec("__NNAP_TRAINER_norm_vec__ = [torch.tensor(sub_vec.asList(), dtype=torch.float64) for sub_vec in __NNAP_TRAINER_norm_vec_j__]");
-        SP.Python.setValue("__NNAP_TRAINER_train_data_base_j__", mTrainDataBaseMat);
-        SP.Python.exec("__NNAP_TRAINER_train_data_base__ = [torch.tensor(sub_base.asListRows(), dtype=torch.float64) for sub_base in __NNAP_TRAINER_train_data_base_j__]");
-        Vector tTrainDataEng = mTrainDataEng.asVec();
-        SP.Python.setValue("__NNAP_TRAINER_train_data_eng_j__", tTrainDataEng);
-        SP.Python.exec("__NNAP_TRAINER_train_data_eng__ = torch.tensor(__NNAP_TRAINER_train_data_eng_j__.asList(), dtype=torch.float64)");
-        IntVector tTrainDataAtomNum = mTrainDataAtomNum.asVec();
-        SP.Python.setValue("__NNAP_TRAINER_train_data_atom_num_j__", tTrainDataAtomNum);
-        SP.Python.exec("__NNAP_TRAINER_train_data_atom_num__ = torch.tensor(__NNAP_TRAINER_train_data_atom_num_j__.asList(), dtype=torch.float64)");
+        SP.Python.setValue(VAL_NORM_VEC_J, mNormVec);
+        SP.Python.exec(VAL_NORM_VEC+" = [torch.tensor(sub_vec.asList(), dtype=torch.float64) for sub_vec in "+VAL_NORM_VEC_J+"]");
+        SP.Python.setValue(VAL_TRAIN_BASE_J, mTrainBaseMat);
+        SP.Python.exec(VAL_TRAIN_BASE+" = [torch.tensor(sub_base.asListRows(), dtype=torch.float64) for sub_base in "+VAL_TRAIN_BASE_J+"]");
+        Vector tTrainDataEng = mTrainEng.asVec();
+        SP.Python.setValue(VAL_TRAIN_ENG_J, tTrainDataEng);
+        SP.Python.exec(VAL_TRAIN_ENG+" = torch.tensor("+VAL_TRAIN_ENG_J+".asList(), dtype=torch.float64)");
+        IntVector tTrainDataAtomNum = mTrainAtomNum.asVec();
+        SP.Python.setValue(VAL_TRAIN_ATOM_NUM_J, tTrainDataAtomNum);
+        SP.Python.exec(VAL_TRAIN_ATOM_NUM+" = torch.tensor("+VAL_TRAIN_ATOM_NUM_J+".asList(), dtype=torch.float64)");
         //noinspection ConcatenationWithEmptyString
         SP.Python.exec("" +
-        "for __NNAP_TRAINER_base__, __NNAP_TRAINER_sub_norm_vec__ in zip(__NNAP_TRAINER_train_data_base__, __NNAP_TRAINER_norm_vec__):\n" +
-        "    for __NNAP_TRAINER_i__ in range(len(__NNAP_TRAINER_base__)):\n" +
-        "        __NNAP_TRAINER_base__[__NNAP_TRAINER_i__, :-1] /= __NNAP_TRAINER_sub_norm_vec__\n" +
-        "del __NNAP_TRAINER_base__, __NNAP_TRAINER_sub_norm_vec__, __NNAP_TRAINER_i__"
+        "for "+VAL_SUB_BASE+", "+VAL_SUB_NORM_VEC+" in zip("+VAL_TRAIN_BASE+", "+VAL_NORM_VEC+"):\n" +
+        "    for "+VAL_I+" in range(len("+VAL_SUB_BASE+")):\n" +
+        "        "+VAL_SUB_BASE+"["+VAL_I+", :-1] /= "+VAL_SUB_NORM_VEC+"\n" +
+        "del "+VAL_SUB_BASE+", "+VAL_SUB_NORM_VEC+", "+VAL_I
         );
-        SP.Python.exec("__NNAP_TRAINER_train_data_eng__ /= __NNAP_TRAINER_train_data_atom_num__");
-        SP.Python.removeValue("__NNAP_TRAINER_norm_vec_j__");
-        SP.Python.removeValue("__NNAP_TRAINER_train_data_base_j__");
-        SP.Python.removeValue("__NNAP_TRAINER_train_data_eng_j__");
-        SP.Python.removeValue("__NNAP_TRAINER_train_data_atom_num_j__");
+        SP.Python.exec(VAL_TRAIN_ENG+" /= "+VAL_TRAIN_ATOM_NUM);
+        SP.Python.removeValue(VAL_NORM_VEC_J);
+        SP.Python.removeValue(VAL_TRAIN_BASE_J);
+        SP.Python.removeValue(VAL_TRAIN_ENG_J);
+        SP.Python.removeValue(VAL_TRAIN_ATOM_NUM_J);
         // 开始训练
         if (aPrintLog) UT.Timer.progressBar("train", aEpochs);
-        SP.Python.exec("__NNAP_TRAINER_model__.train()");
+        SP.Python.exec(VAL_MODEL+".train()");
         for (int i = 0; i < aEpochs; ++i) {
-            double tLoss = ((Number)SP.Python.invoke("__NNAP_TRAINER_train_step__")).doubleValue();
+            double tLoss = ((Number)SP.Python.invoke(FN_TRAIN_STEP)).doubleValue();
             if (aPrintLog) UT.Timer.progressBar(String.format("loss: %.4g", tLoss));
         }
         // 完事移除临时数据
-        SP.Python.removeValue("__NNAP_TRAINER_norm_vec__");
-        SP.Python.removeValue("__NNAP_TRAINER_train_data_base__");
-        SP.Python.removeValue("__NNAP_TRAINER_train_data_eng__");
-        SP.Python.removeValue("__NNAP_TRAINER_train_data_atom_num__");
+        SP.Python.removeValue(VAL_NORM_VEC);
+        SP.Python.removeValue(VAL_TRAIN_BASE);
+        SP.Python.removeValue(VAL_TRAIN_ENG);
+        SP.Python.removeValue(VAL_TRAIN_ATOM_NUM);
     }
     
     @ApiStatus.Internal
@@ -273,7 +298,7 @@ public class Trainer {
         // 这里也重新排序原子数据的 symbol 顺序
         NNAP.reorderSymbols_(AbstractCollections.from(mSymbols), aAtomData);
         // 添加数据
-        calRefEngBaseAndAdd_(aAtomData, aEnergy, mTrainDataEng, mTrainDataBase);
-        mTrainDataAtomNum.add(aAtomData.atomNumber());
+        calRefEngBaseAndAdd_(aAtomData, aEnergy, mTrainEng, mTrainBase);
+        mTrainAtomNum.add(aAtomData.atomNumber());
     }
 }
