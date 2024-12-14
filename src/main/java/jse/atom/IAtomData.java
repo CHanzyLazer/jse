fbd3f3051c3bfe89660f7f0aba6da821a3f5da72
CHanzyLazer <chanzylazer@qq.com>
2024-12-14 15:40:08 +0800

    原子数据文档重写

--- src/main/java/jse/atom/IAtomData.java
+++ src/main/java/jse/atom/IAtomData.java
@@ -8,71 +8,194 @@ import org.jetbrains.annotations.VisibleForTesting;
 import java.util.List;
 
 /**
+ * 通用的原子数据接口，此接口只开放了访问接口，没开放修改接口
+ * <p>
+ * 一般来说，通过 {@link #atoms()} 来获取内部原子组成的列表，通过 {@link #atom(int)}
+ * 来获取指定索引的原子，有关系 {@code atoms()[i] == atom(i)}
+ * <p>
+ * jse 的原子原则上采用 lammps 的类似定义，即除了原子的 {@code xyz} 坐标外，还包含一个原子的 {@code id}
+ * 以及原子的种类编号 {@code type}，其中 {@code id} 和 {@code type} 和 lammps
+ * 保持一致也从 {@code 1} 开始索引（保证读取 lammps data 文件后获取的结果一致）；{@code id}
+ * 可以有任意的顺序，并且可以不连续，而 {@code type} 则会保证 {@code data.atom(i).type() <= data.ntypes()}；
+ * 注意区分原子本身的 {@code id} 以及原子处于原子数据的位置索引 {@code idx}
+ * <p>
+ * jse 的原子数据统一在一个从原点开始的模拟盒内，因此一般来说不会出现坐标为负值的原子；
+ * 对于存在下边界模拟盒的原子数据（例如 lammps 的 data），这里获取到的坐标会自动减去这个下边界。
+ *
+ * @see ISettableAtomData ISettableAtomData，对于可以修改的原子数据
+ * @see AtomData AtomData，对于原子数据的默认实现
+ * @see IAtom IAtom，对于 jse 中的原子定义
+ * @see IBox IBox，对于 jse 中的模拟盒定义
  * @author liqa
- * <p> 通用的拥有原子数据的类使用的接口，主要用于相互转换 </p>
- * <p> 移除过多的无用的接口，只保留实际使用会用到的部分 </p>
  */
 public interface IAtomData {
-    /** 转为兼容性更高的 double[][]，约定数据按行排列，每行一个原子；现在默认 data() 顺序调整，改为 x, y, z, id, type, vx, vy, vz，将最基础的放在最前面可以保证最好的兼容性 */
+    /**
+     * 直接获取 {@code double[][]} 数据，会进行一次值拷贝
+     * @return 按行排列的数组，每行对应一个原子，
+     * 顺序为 {@code x, y, z, id, type, vx, vy, vz}
+     * @see IAtom#data()
+     */
     double[][] data();
+    /**
+     * 直接获取 {@code double[][]} 数据，会进行一次值拷贝
+     * @return 按行排列的数组，每行对应一个原子，
+     * 顺序为 {@code x, y, z}
+     * @see IAtom#dataXYZ()
+     */
     double[][] dataXYZ();
+    /**
+     * 直接获取 {@code double[][]} 数据，会进行一次值拷贝
+     * @return 按行排列的数组，每行对应一个原子，
+     * 顺序为 {@code x, y, z, id}
+     * @see IAtom#dataXYZID()
+     */
     double[][] dataXYZID();
+    /**
+     * 直接获取 {@code double[][]} 数据，会进行一次值拷贝
+     * @return 按行排列的数组，每行对应一个原子，
+     * 顺序为 {@code id, type, x, y, z}
+     * @see IAtom#dataSTD()
+     */
     double[][] dataSTD();
+    /**
+     * 直接获取 {@code double[][]} 数据，会进行一次值拷贝
+     * @return 按行排列的数组，每行对应一个原子，
+     * 顺序为 {@code id, type, x, y, z, vx, vy, vz}
+     * @see IAtom#dataAll()
+     */
     double[][] dataAll();
-    /** 获取速度数据, vx, vy, vz */
+    /**
+     * 直接获取 {@code double[][]} 数据，会进行一次值拷贝
+     * @return 按行排列的数组，每行对应一个原子，
+     * 顺序为 {@code vx, vy, vz}
+     * @see IAtom#dataVelocities()
+     */
     double[][] dataVelocities();
+    
+    /**
+     * @return 此原子数据是否真实包含速度信息
+     * @see IAtom#hasVelocity()
+     */
     boolean hasVelocity();
     /** @deprecated use {@link #hasVelocity} */
     @Deprecated default boolean hasVelocities() {return hasVelocity();}
     
-    /** 现在改为 atoms 保证一致性 */
+    /**
+     * @return 内部原子引用组成的列表
+     * @see IAtom
+     */
     List<? extends IAtom> atoms();
-    /** 现在统一提供随机访问获取一个原子的接口；改为 atom 保证逻辑一致，旧名称由于外部使用较少不再保留 */
-    IAtom atom(int aIdx);
-    
     /** @deprecated use {@link #atoms} */
     @Deprecated default List<? extends IAtom> asList() {return atoms();}
+    /**
+     * 直接获取指定索引的原子，可以避免一次创建匿名列表的过程；
+     * 有关系 {@code atoms()[i] == atom(i)}
+     * @param aIdx 需要获取原子的索引
+     * @return 对应索引的原子引用
+     * @see IAtom
+     */
+    IAtom atom(int aIdx);
     
-    /** 保留获取原子总数的接口，但是特定种类的原子数目现在不能直接获取 */
+    /** @return 原子总数 */
     int atomNumber();
-    int atomTypeNumber();
-    /** @deprecated use {@link #atomNumber} or {@link #natoms} */     @Deprecated default int atomNum() {return atomNumber();}
-    /** @deprecated use {@link #atomTypeNumber} or {@link #ntypes} */ @Deprecated default int atomTypeNum() {return atomTypeNumber();}
-    /** 提供简写版本 */
+    /** @see #atomNumber() */
     @VisibleForTesting default int natoms() {return atomNumber();}
-    @VisibleForTesting default int ntypes() {return atomTypeNumber();}
+    /** @deprecated use {@link #atomNumber} or {@link #natoms} */
+    @Deprecated default int atomNum() {return atomNumber();}
     
-    /** 获取模拟盒信息的接口，现在为了使用方便，移除了 boxLo 的设定 */
+    /** @return 原子种类的总数，可以大于实际真实包含的原子种类数目 */
+    int atomTypeNumber();
+    /** @see #atomTypeNumber() */
+    @VisibleForTesting default int ntypes() {return atomTypeNumber();}
+    /** @deprecated use {@link #atomTypeNumber} or {@link #ntypes} */
+    @Deprecated default int atomTypeNum() {return atomTypeNumber();}
+    
+    /**
+     * @return 此原子数据对应的模拟盒
+     * @see IBox
+     */
     IBox box();
+    /**
+     * @return 此原子数据模拟盒的体系
+     * @see IBox#volume()
+     */
     default double volume() {return box().volume();}
+    /**
+     * @return 此原子数据模拟盒是否是三斜的
+     * @see IBox#isPrism()
+     */
     default boolean isPrism() {return box().isPrism();}
+    /**
+     * @return 此原子数据模拟盒是否是下三角的（满足 lammps 的风格）
+     * @see IBox#isLmpStyle()
+     */
     default boolean isLmpStyle() {return box().isLmpStyle();}
     
-    /** 现在都能获取到元素信息和质量，不存在的会返回 null */
+    /**
+     * @return 此原子数据是否包含元素符号信息
+     * @see IAtom#hasSymbol()
+     */
     boolean hasSymbol();
+    /**
+     * @return 按照原子种类编号排序的元素符号列表，有
+     * {@code symbols().size() == ntypes()}；
+     * 如果不存在元素符号信息则会返回 {@code null}
+     * @see IAtom#symbol()
+     */
     @Nullable List<@Nullable String> symbols();
+    /**
+     * 直接获取指定种类编号下的元素符号，可以避免一次创建匿名列表的过程；
+     * 有关系 {@code symbol(type) == symbols()[type-1]}
+     * @param aType 需要查询的种类编号，从 {@code 1} 开始
+     * @return 指定种类编号的元素符号，如果不存在元素符号信息则会返回 {@code null}
+     * @see IAtom#symbol()
+     * @see IAtom#type()
+     */
     @Nullable String symbol(int aType);
+    
+    /**
+     * @return 此原子数据是否包含质量信息
+     * @see IAtom#hasMass()
+     */
     boolean hasMass();
+    /**
+     * @return 按照原子种类编号排序的质量向量，有
+     * {@code masses().size() == ntypes()}；
+     * 如果不存在质量信息则会返回 {@code null}
+     * @see IVector
+     * @see IAtom#mass()
+     */
     @Nullable IVector masses();
+    /**
+     * 直接获取指定种类编号下的质量，可以避免一次创建匿名列表的过程；
+     * 有关系 {@code mass(type) == masses()[type-1]}
+     * @param aType 需要查询的种类编号，从 {@code 1} 开始
+     * @return 指定种类编号的质量，如果不存在质量信息则会返回 {@link Double#NaN}
+     * @see IAtom#mass()
+     * @see IAtom#type()
+     */
     double mass(int aType);
     
-    /** 统一提供拷贝接口 */
+    /** @return 此原子数据的拷贝，拷贝结果都是可以修改的 */
     ISettableAtomData copy();
     
-    /** 返回原子数据计算器，现在不再使用专门的 Generator */
+    /**
+     * 获取原子数据的计算器，包含许多较为复杂的原子数据操作
+     * <p>
+     * 例如对当前原子数据进行扩胞：
+     * <pre> {@code
+     * def repeatData = data.operation().repeat(2, 3, 4)
+     * } </pre>
+     * @return 原子数据计算器
+     * @see IAtomDataOperation
+     * @see ISettableAtomDataOperation
+     */
     IAtomDataOperation operation();
+    /** @see #operation() */
     @VisibleForTesting default IAtomDataOperation opt() {return operation();}
     
     
-    /// 实用功能，这里依旧保留这种写法
-    /**
-     * 获取单原子参数的计算器，支持使用 MPC 的简写来调用
-     * @param aType 指定此值来获取只有这个种类的原子的单原子计算器，用于计算只考虑一种元素的一些参数
-     * @param aThreadNum 执行 MPC 的线程数目
-     * @return 获取到的 MPC
-     * @deprecated use {@link MonatomicParameterCalculator#of} or {@link MPC#of}
-     */
-    @Deprecated default MonatomicParameterCalculator getTypeMonatomicParameterCalculator(int aType, @Range(from=1, to=Integer.MAX_VALUE) int aThreadNum) {return MonatomicParameterCalculator.of(operation().filterType(aType), aThreadNum);}
+    /** @deprecated use {@link MonatomicParameterCalculator#of}*/ @Deprecated default MonatomicParameterCalculator getTypeMonatomicParameterCalculator(int aType, @Range(from=1, to=Integer.MAX_VALUE) int aThreadNum) {return MonatomicParameterCalculator.of(operation().filterType(aType), aThreadNum);}
     /** @deprecated use {@link MonatomicParameterCalculator#of}*/ @Deprecated default MonatomicParameterCalculator getMonatomicParameterCalculator    (                                                              ) {return MonatomicParameterCalculator.of(this                                     );}
     /** @deprecated use {@link MonatomicParameterCalculator#of}*/ @Deprecated default MonatomicParameterCalculator getMonatomicParameterCalculator    (           @Range(from=1, to=Integer.MAX_VALUE) int aThreadNum) {return MonatomicParameterCalculator.of(this                         , aThreadNum);}
     /** @deprecated use {@link MonatomicParameterCalculator#of}*/ @Deprecated default MonatomicParameterCalculator getTypeMonatomicParameterCalculator(int aType                                                     ) {return MonatomicParameterCalculator.of(operation().filterType(aType)            );}
