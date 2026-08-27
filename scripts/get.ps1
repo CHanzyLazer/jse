599156d51285f8c47a10bcaae1cf8a12e5676bfe
Qing'an Li <chanzylazer@qq.com>
2026-08-27 13:47:21 +0800

    4.2.0

--- scripts/get.ps1
+++ scripts/get.ps1
@@ -10,7 +10,7 @@ param (
 )
 
 & {
-    $LastRelease = "4.1.2"
+    $LastRelease = "4.2.0"
     $ErrorActionPreference = 'Stop'
     
     $WorkingDir = Join-Path ([System.IO.Path]::GetTempPath()) ("jse-getter-" + [guid]::NewGuid().ToString())
